47374f65e4c7f9de6f6030207dd23e92be0d664c
Floh1983 <florian.schlosser@comline.de>
2024-05-13 09:59:16 +0200

    Prealpha

--- src/main/java/org/fs/jirabackup/Backing.java
+++ src/main/java/org/fs/jirabackup/Backing.java
@@ -57,22 +57,3 @@ public class Backing {
     }
 
 }
-
-OkHttpClient client = new OkHttpClient();
-
-Request request = new Request.Builder()
-        .url("https://jira.comline.de/sr/jira.issueviews:searchrequest-fullcontent/temp/SearchRequest.html?jqlQuery=key+%3D+AEGHAW-2381")
-        .get()
-        .addHeader("Authorization", "Basic c2NobG9zc2VyOk1lcGhpc3RvMX5qYW4=")
-        .addHeader("User-Agent", "PostmanRuntime/7.13.0")
-        .addHeader("Accept", "*/*")
-        .addHeader("Cache-Control", "no-cache")
-        .addHeader("Postman-Token", "12ea7ce3-0c65-4de0-b3fa-15117a9ed3d2,32c799e6-68a5-4171-a644-ff65e5d71bc7")
-        .addHeader("Host", "jira.comline.de")
-        .addHeader("cookie", "JSESSIONID=11C45299F2967C3BDF1460C75DCD9147; atlassian.xsrf.token=B2LQ-2W30-5PCA-3L89|d890cd29de469b59491c199ec759157f37e6aa2c|lin")
-        .addHeader("accept-encoding", "gzip, deflate")
-        .addHeader("Connection", "keep-alive")
-        .addHeader("cache-control", "no-cache")
-        .build();
-
-Response response = client.newCall(request).execute();
